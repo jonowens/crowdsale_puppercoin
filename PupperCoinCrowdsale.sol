9f1a50a20df11325760c956a3773670423e6a1da
jowens520 <jonathan.owens@gmail.com>
2021-02-23 19:32:45 -0600

    Created separate variable for wei cap.

--- PupperCoinCrowdsale.sol
+++ PupperCoinCrowdsale.sol
@@ -13,15 +13,16 @@ contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCro
     constructor(
         address payable wallet,
         uint goal,
+        uint cap,
         uint rate,
         PupperCoin token,
         uint openTime,
         uint closeTime
         )
-        Crowdsale(rate, wallet, token)
-        CappedCrowdsale(goal)
+        CappedCrowdsale(cap)
         TimedCrowdsale(openTime, closeTime)
         RefundableCrowdsale(goal)
+        Crowdsale(rate, wallet, token)
         public {
         // constructor can stay empty
     }
@@ -35,13 +36,19 @@ contract PupperCoinSaleDeployer {
     constructor(
         string memory name,
         string memory symbol,
-        address payable wallet) public {
+        address payable wallet,
+        uint goal
+        )
+        
+        public
+        
+        {
         // Create the PupperCoin and keep its address handy
         PupperCoin token = new PupperCoin(name, symbol, 0);
         tokenAddress = address(token);
 
         // Create the PupperCoinSale and tell it about the token, set the goal = 300, and set the open and close times to now and now + 24 weeks.
-        PupperCoinSale tokenSale = new PupperCoinSale(wallet, 300, 1, token, now, now + 24 weeks);
+        PupperCoinSale tokenSale = new PupperCoinSale(wallet, goal, 300, 1, token, now, now + 10 minutes);
         tokenSaleAddress = address(tokenSale);
 
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
